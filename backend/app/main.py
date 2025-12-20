3c1c773e2bffeb273ae6864a0471a68d99fb559b
Sachin10-git <sachins210194@gmail.com>
2025-12-20 23:21:49 +0530

    fix main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,11 +36,3 @@ def health():
     }
 
 
-# Local run
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=8000,
-    )
