d790f3d2c5cc8b96d9859fa1ef6b9d3c3de1b96c
Sachin10-git <sachins210194@gmail.com>
2025-12-20 22:19:39 +0530

    update backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://ai-fraud-shield-backend.onrender.com";
 
 export async function predictTransaction(payload) {
   const response = await fetch(`${BASE_URL}/api/model/predict`, {
